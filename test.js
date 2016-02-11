214c3d99e5620ea819c43a6e0320a2f98941a867
Andrew Buttigieg <andrewbuttigieg@gmail.com>
2016-02-11 21:09:06 +0100

    start using should on json objects

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 var supertest = require("supertest");
 var should = require("should");
+var expect = require("expect");
 
 // This agent refers to PORT where program is runninng.
 
@@ -45,7 +46,7 @@ describe("Docs unit test",function(){
 });
 
 describe("Menu unit test",function(){
-  it("should return the docs page",function(done){
+  it("should return the menu and menu should have some sports",function(done){
     server
     .get("/menu/get?language=en")
     .expect("Content-type",/json/)
@@ -53,8 +54,24 @@ describe("Menu unit test",function(){
     .end(function(err,res){
       // HTTP status should be 200
       res.status.should.equal(200);
-      // Error key should be false.
-      //res.body.error.should.equal(false);
+      //check if we have sports..
+      should(res.body.sports.length > 5).be.equal(true);
+      done();
+    });
+  });
+});
+
+describe("Prelive unit test",function(){
+  it("should return the menu and menu should have some sports",function(done){
+    server
+    .get("/prelive/get?language=en")
+    .expect("Content-type",/json/)
+    .expect(200) // THis is HTTP response
+    .end(function(err,res){
+      // HTTP status should be 200
+      res.status.should.equal(200);
+      //check if we have sports..
+      should(res.body.PreliveMatches.length > 50).be.equal(true);
       done();
     });
   });
